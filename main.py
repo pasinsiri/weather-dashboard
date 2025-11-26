d50ee526d6d950950e86008e6a04c301f44ac199
pasinsiri <pasin_plp0206@hotmail.com>
2025-11-26 18:06:23 +0700

    handle get requests

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/")
+async def home(request: Request):
+    location = get_location_from_ip()
+    weather = await get_weather(location, os.getenv("OPENWEATHER_API_KEY"))
+    return templates.TemplateResponse("index.html", {
+        "request": request,
+        "weather": weather,
+        "location": location
+    })
+
